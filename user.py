73a498379cc2588c4d87c12bf29334a55f72fa47
iyked3 <154082030+iyked3@users.noreply.github.com>
2024-02-23 22:36:53 +0100

    Add files via upload

---
+++ user.py
@@ -0,0 +1,7 @@
+name = input("please enter your name: ")
+age = input("please enter your age: ")
+location = input("please enter your location: ")
+
+print("Name:", name)
+print("Age: ", age)
+print("Location: ", location)
