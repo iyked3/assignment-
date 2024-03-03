5e37db3f5661c7c36dec99e8d24d58c713b1067c
iyked3 <154082030+iyked3@users.noreply.github.com>
2024-03-03 12:39:52 +0100

    Add files via upload

---
+++ user.py
@@ -0,0 +1,12 @@
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(1, 15)
+my_list.extend([50, 60, 70])
+my_list.remove(70)
+my_list.sort()
+print(my_list[3])
+print(my_list)
+
