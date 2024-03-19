bb0b19d45ef643ffb1210b47fe511694b6861f9a
iyked3 <154082030+iyked3@users.noreply.github.com>
2024-03-19 20:27:04 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+    
+    def introduce(self):
+        print(f"Hello, my name is {self.name}. I am {self.age} years old and I am {self.gender}.")
+
+
+Person1 = Person("Dennis", 21, "male")
+
+
+Person1.introduce()
